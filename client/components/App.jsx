05453d288eabaa273162f9cdfe02bdd4094c7986
sam-drumm <samueldrumm@gmail.com>
2022-02-11 11:15:14 +1300

    Add comps and routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,26 +1,33 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { fetchFruits } from '../actions'
+import AddItem from './AddItem'
+import ListItem from './Listitem'
+
+import { Route, Routes } from 'react-router-dom'
+
+// import { fetchFruits } from '../actions'
 
 function App () {
-  const fruits = useSelector(state => state.fruits)
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(fetchFruits())
-  }, [])
+  // const fruits = useSelector(state => state.fruits)
+  // const dispatch = useDispatch()
+  // useEffect(() => {
+  //   dispatch(fetchFruits())
+  // }, [])
 
   return (
     <>
       <div className='app'>
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>
-          {fruits.map(fruit => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
+        <header>Hello this is your header</header>
+        <Routes>
+          <Route path='/' element={<ListItem />} />
+          <Route path='/bah' element={<AddItem />} />
+        </Routes>
+        <footer>Goodbye this is your footer</footer>
+
       </div>
     </>
+
   )
 }
 
