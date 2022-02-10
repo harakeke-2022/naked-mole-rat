f4a4f3068c9a036092e8f84de2027b9071926fe4
Ningyi Hu <nicole005213@gmail.com>
2022-02-11 11:19:59 +1300

    put in notes

--- server/db/seeds/notes.js
+++ server/db/seeds/notes.js
@@ -4,9 +4,9 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('notes').insert([
-        { id: 1, title: 'Greeting', note: 'hello note app' },
-        { id: 2, title: 'jinny talk', note: 'English is shit' },
-        { id: 3, title: 'burger geek', note: 'yummmmmm' }
+        { id: 1, title: 'Greeting', note: 'hello notes app' },
+        { id: 2, title: 'jinny and ningyi', note: 'are bilingual so english be hard for them sometimes' },
+        { id: 3, title: 'burger geek', note: 'yum' }
       ])
     })
 }
